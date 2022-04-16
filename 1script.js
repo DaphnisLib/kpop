a743d48c99ccd30f0db052383c4fc3d45b319022
DaphnisLib <1298619120@qq.com>
2022-04-17 00:03:58 +0800

    Initial commit

---
+++ 1script.js
@@ -0,0 +1,78 @@
+var i=[];
+i.spring("img/spring.gif");
+i.summer("img/summer.gif");
+i.autumn("img/autumn.gif");
+i.winter("img/winter.gif");
+
+
+function spring(){
+document.getElementById("img").src="spring.gif";
+}
+function summer(){
+document.getElementById("img").src="summer.gif";
+}
+function autumn(){
+document.getElementById("img").src="autumn.gif";
+}
+function winter(){
+document.getElementById("img").src="winter.gif";
+}
+
+function Jan(){
+document.getElementById("images").src="Jan.gif";
+}
+function Feb(){
+document.getElementById("images").src="Feb.gif";
+}
+function Mar(){
+document.getElementById("images").src="Mar.gif";
+}
+function Apr(){
+document.getElementById("images").src="Apr.gif";
+}
+function May(){
+document.getElementById("images").src="May.gif";
+}
+function Jun(){
+document.getElementById("images").src="Jun.gif";
+}
+function Jul(){
+document.getElementById("images").src="Jul.gif";
+}
+function Aug(){
+document.getElementById("images").src="Aug.gif";
+}
+function Sept(){
+document.getElementById("images").src="Sept.gif";
+}
+function Oct(){
+document.getElementById("images").src="Oct.gif";
+}
+function Nov(){
+document.getElementById("images").src="Nov.gif";
+}
+function Dec(){
+document.getElementById("images").src="Dec.gif";
+}
+
+function Sunday(){
+document.getElementById("gif").src="Sunday.gif";
+}
+function Monday(){
+document.getElementById("gif").src="Monday.gif";
+}
+function Tuesday(){
+document.getElementById("gif").src="Tuesday.gif";
+}
+function Wednesday(){
+document.getElementById("gif").src="Wednesday.gif";
+}
+function Thursday(){
+document.getElementById("gif").src="Thursday.gif";
+}
+function Friday(){
+document.getElementById("gif").src="Friday.gif";
+}
+function Saturday(){
+document.getElementById("gif").src="Saturday.gif";
+}
